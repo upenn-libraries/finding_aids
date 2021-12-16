786ba89663a3098e1de33906d5335a3e4fddacaf
Mike Kanning <mkanning@upenn.edu>
2021-12-16 17:18:20 -0500

    sort status page by most recently updated

--- ansible/roles/finding_aid_discovery/files/src/app/controllers/endpoints_controller.rb
+++ ansible/roles/finding_aid_discovery/files/src/app/controllers/endpoints_controller.rb
@@ -1,6 +1,6 @@
 class EndpointsController < ApplicationController
   layout 'application'
   def index
-    @endpoints = Endpoint.all
+    @endpoints = Endpoint.all.order(updated_at: :desc)
   end
 end
