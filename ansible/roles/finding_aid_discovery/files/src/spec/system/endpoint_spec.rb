7a4c87f42d0c25b8284b27c7c24ec32fa3091683
Mike Kanning <mkanning@upenn.edu>
2022-01-07 14:34:34 -0500

    add row scope to some Endpoint system specs to prevent false positives

--- ansible/roles/finding_aid_discovery/files/src/spec/system/endpoint_spec.rb
+++ ansible/roles/finding_aid_discovery/files/src/spec/system/endpoint_spec.rb
@@ -25,19 +25,27 @@ describe 'Endpoint dashboard' do
     end
     it 'renders all endpoint slugs' do
       test_endpoints.each do |endpoint|
-        expect(page).to have_text endpoint.slug
+        within ".table-row-#{endpoint.slug}" do
+          expect(page).to have_text endpoint.slug
+        end
       end
     end
     it 'renders links to endpoint show pages' do
       test_endpoints.each do |endpoint|
-        expect(page).to have_link endpoint.slug, href: endpoint_path(endpoint.slug)
+        within ".table-row-#{endpoint.slug}" do
+          expect(page).to have_link endpoint.slug, href: endpoint_path(endpoint.slug)
+        end
       end
     end
     it 'shows error message text for problem harvest' do
-      expect(page).to have_text endpoint_failed.last_harvest_errors.first
+      within ".table-row-#{endpoint_failed.slug}" do
+        expect(page).to have_text endpoint_failed.last_harvest_errors.first
+      end
     end
     it 'shows a count of removed records' do
-      expect(page).to have_text "removed #{endpoint_removals.last_harvest_removed_ids.count} record"
+      within ".table-row-#{endpoint_removals.slug}" do
+        expect(page).to have_text "removed #{endpoint_removals.last_harvest_removed_ids.count} record"
+      end
     end
   end
   context 'show' do
