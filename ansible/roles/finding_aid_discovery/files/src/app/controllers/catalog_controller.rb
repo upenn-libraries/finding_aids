a378b64d08fac58dc564e5c3a8b08642bce441a5
Mike Kanning <mkanning@upenn.edu>
2022-01-31 16:52:52 -0500

    show newly added fields and facets. update labels for some fields.

--- ansible/roles/finding_aid_discovery/files/src/app/controllers/catalog_controller.rb
+++ ansible/roles/finding_aid_discovery/files/src/app/controllers/catalog_controller.rb
@@ -81,10 +81,14 @@ class CatalogController < ApplicationController
 
     config.add_facet_field 'endpoint_ssi', label: I18n.t('fields.endpoint')
     config.add_facet_field 'repository_ssi', label: I18n.t('fields.repository')
-    config.add_facet_field 'people_ssim', label: I18n.t('fields.people')
-    config.add_facet_field 'corpnames_ssim', label: I18n.t('fields.corpnames')
     config.add_facet_field 'subjects_ssim', label: I18n.t('fields.subjects')
+    config.add_facet_field 'corpnames_ssim', label: I18n.t('fields.corpnames')
+    config.add_facet_field 'people_ssim', label: I18n.t('fields.people')
     config.add_facet_field 'places_ssim', label: I18n.t('fields.places')
+    config.add_facet_field 'genre_form_ssim', label: I18n.t('fields.genre_form')
+    config.add_facet_field 'creator_ssim', label: I18n.t('fields.creators')
+    config.add_facet_field 'donors_ssim', label: I18n.t('fields.donors')
+    config.add_facet_field 'languages_ssim', label: I18n.t('fields.language')
 
     config.add_facet_fields_to_solr_request!
 
@@ -95,14 +99,23 @@ class CatalogController < ApplicationController
 
     config.add_show_field 'ead_id_ssi', label: I18n.t('fields.internal_id')
     config.add_show_field 'unit_id_ssi', label: I18n.t('fields.ead_id')
+    config.add_show_field 'pretty_unit_id_ssi', label: I18n.t('fields.pretty_unit_id')
     config.add_show_field 'title_tsi', label: I18n.t('fields.title')
-    config.add_show_field 'repository_ssim', label: I18n.t('fields.repository')
+    config.add_show_field 'repository_ssi', label: I18n.t('fields.repository'), link_to_facet: true
     config.add_show_field 'extent_ssi', label: I18n.t('fields.extent')
+    config.add_show_field 'languages_ssim', label: I18n.t('fields.language'), link_to_facet: true
+    config.add_show_field 'preferred_citation_ss', label: I18n.t('fields.citation')
+
     config.add_show_field 'inclusive_date_ss', label: I18n.t('fields.date')
-    config.add_show_field 'people_ssim', label: I18n.t('fields.people')
-    config.add_show_field 'corpnames_ssim', label: I18n.t('fields.corpnames')
-    config.add_show_field 'subjects_ssim', label: I18n.t('fields.subjects')
-    config.add_show_field 'places_ssim', label: I18n.t('fields.places')
+    config.add_show_field 'bulk_date_ss', label: I18n.t('fields.bulk_date')
+
+    config.add_show_field 'people_ssim', label: I18n.t('fields.people'), link_to_facet: true
+    config.add_show_field 'corpnames_ssim', label: I18n.t('fields.corpnames'), link_to_facet: true
+    config.add_show_field 'creator_ssim', label: I18n.t('fields.creators'), link_to_facet: true
+    config.add_show_field 'subjects_ssim', label: I18n.t('fields.subjects'), link_to_facet: true
+    config.add_show_field 'places_ssim', label: I18n.t('fields.places'), link_to_facet: true
+    config.add_show_field 'donors_ssim', label: I18n.t('fields.donors'), link_to_facet: true
+    config.add_show_field 'genre_form_ssim', label: I18n.t('fields.genre_form'), link_to_facet: true
     config.add_show_field 'abstract_scope_contents_tsi', label: I18n.t('fields.abstract_scope_contents')
     config.add_show_field 'xml_ss', label: I18n.t('fields.raw_xml')
 
