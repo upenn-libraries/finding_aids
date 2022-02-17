53082a41e6dd95903f8dcdc2450a5acbed1f7ce4
Mike Kanning <mkanning@upenn.edu>
2022-02-17 16:30:28 -0500

    build out support for additional markup tags found in the ead render attribute - because HTML isn't good enough, amirite?

--- ansible/roles/finding_aid_discovery/files/src/spec/components/ead_markup_translation_component_spec.rb
+++ ansible/roles/finding_aid_discovery/files/src/spec/components/ead_markup_translation_component_spec.rb
@@ -26,31 +26,74 @@ XML
     end
 
     context 'with formatting from render attribute or emph node' do
-      let(:xml) do
-        <<XML
+      context 'with single formatting declarations' do
+        let(:xml) do
+          <<XML
           <parent>
             <title render="underline">Underlined</title>
             <title render="super">Superscript</title>
             <title render="sub">Subscript</title>
             <title render="bold">Bolded</title>
             <title render="italic">Italicized</title>
+            <title render="smcaps">Small Caps</title>
+            <title render="doublequote">Double Quotes</title>
+            <title render="singlequote">Single Quotes</title>
             <emph>Italicized</emph>
           </parent>
 XML
+        end
+
+        it 'applies underline markup and class accordingly' do
+          expect(rendered_component).to have_xpath '//span[@class="underline"]', count: 1
+        end
+
+        it 'applies sub- and super-script markup accordingly' do
+          expect(rendered_component).to have_xpath '//sup', count: 1
+          expect(rendered_component).to have_xpath '//sub', count: 1
+        end
+
+        it 'applies markup for bolded text' do
+          expect(rendered_component).to have_xpath '//strong', count: 1
+        end
+
+        it 'applies markup for italicized text' do
+          expect(rendered_component).to have_xpath '//em', count: 2
+        end
+
+        it 'applies markup for small-caps text' do
+          expect(rendered_component).to have_xpath '//span[@class="small-caps"]', count: 1
+        end
+
+        it 'wraps text with quotations accordingly' do
+          expect(rendered_component).to have_xpath '//span', count: 1, text: '"Double Quotes"'
+          expect(rendered_component).to have_xpath '//span', count: 1, text: "'Single Quotes'"
+        end
       end
 
-      it 'applies underline markup and class accordingly' do
-        expect(rendered_component).to have_xpath '//span[@class="underline"]', count: 1
-      end
+      context 'with combined formatting declarations' do
+        let(:xml) do
+          <<XML
+            <parent>
+              <titleproper render="bolditalic">Bolded and Italicized</title>
+              <titleproper render="bolddoublequote">Bolded with Double Quotes</title>
+              <titleproper render="boldsinglequote">Bolded with Single Quotes</title>
+              <title render="boldsmcaps">Bolded Small Caps</title>
+            </parent>
+XML
+        end
 
-      it 'applies sub- and super-script markup accordingly' do
-        expect(rendered_component).to have_xpath '//sup', count: 1
-        expect(rendered_component).to have_xpath '//sub', count: 1
-      end
+        it 'applies combined bold and italic markup accordingly' do
+          expect(rendered_component).to have_xpath '//strong/em', count: 1, text: 'Bolded and Italicized'
+        end
 
-      it 'applies italic markup accordingly for bold and italicized text' do
-        expect(rendered_component).to have_xpath '//strong', count: 1
-        expect(rendered_component).to have_xpath '//em', count: 2
+        it 'applies combined bold and small-caps markup' do
+          expect(rendered_component).to have_xpath '//strong[@class="small-caps"]', count: 1
+        end
+
+        it 'wraps text with quotations accordingly' do
+          expect(rendered_component).to have_xpath '//strong', count: 1, text: '"Bolded with Double Quotes"'
+          expect(rendered_component).to have_xpath '//strong', count: 1, text: "'Bolded with Single Quotes'"
+        end
       end
     end
   end
