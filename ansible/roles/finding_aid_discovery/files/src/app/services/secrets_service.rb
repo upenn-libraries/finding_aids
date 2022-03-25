b144580798f18fece213ffdbb44285f8bbdd8c2f
Mike Kanning <mkanning@upenn.edu>
2022-03-25 10:43:39 -0400

    refine exceptions generated by SecretsService. add specs to validate expected exception behavior.

--- ansible/roles/finding_aid_discovery/files/src/app/services/secrets_service.rb
+++ ansible/roles/finding_aid_discovery/files/src/app/services/secrets_service.rb
@@ -5,14 +5,13 @@ class SecretsService
   PERMITTED_SECRETS = %i[penn_aspace_api_username penn_aspace_api_password].freeze
   SECRET_STORE_PATH = '/run/secrets'
 
-  class SecretsServiceError < StandardError; end
+  class InvalidKeyError < StandardError; end
 
   # @param [Symbol, String] key
   # @param [String, NilClass] default
   # @return [String, NilClass]
   def self.lookup(key:, default: nil)
-    raise SecretsServiceError, 'No secret key defined' if key.blank?
-    raise SecretsServiceError, "Non-permitted secret key provided: #{key}" unless key.to_sym.in? PERMITTED_SECRETS
+    raise InvalidKeyError, "Non-permitted secret key provided: #{key}" unless key.to_sym.in? PERMITTED_SECRETS
 
     secret_file_path = File.join SECRET_STORE_PATH, key.to_s
     if File.exist? secret_file_path
