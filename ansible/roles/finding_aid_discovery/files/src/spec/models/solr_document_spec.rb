832ff655d14d4f175b0906bb873a18f5b1d56313
Carla Galarza <cgalarza@upenn.edu>
2022-04-18 15:51:40 -0400

    Adding test for solr document topics method

--- ansible/roles/finding_aid_discovery/files/src/spec/models/solr_document_spec.rb
+++ ansible/roles/finding_aid_discovery/files/src/spec/models/solr_document_spec.rb
@@ -3,12 +3,24 @@
 require 'rails_helper'
 
 describe SolrDocument do
-  subject(:doc) { described_class.new(xml_ss: xml) }
+  subject(:doc) do
+    described_class.new(
+      xml_ss: xml,
+      places_ssim: ['Philadelphia'],
+      people_ssim: ['Doe, John'],
+      subjects_ssim: ['Cooking'],
+      corpnames_ssim: ['University of Pennsylvania']
+    )
+  end
 
   let(:xml) { file_fixture('ead/penn_museum_ead_1.xml').read }
 
   it 'creates a ParsedEad' do
     expect(doc.parsed_ead).to be_an_instance_of SolrDocument::ParsedEad
+  end
+
+  it 'returns expected topics' do
+    expect(doc.topics).to contain_exactly('Philadelphia', 'Doe, John', 'Cooking', 'University of Pennsylvania')
   end
 
   context 'when using ParsedEad object' do
