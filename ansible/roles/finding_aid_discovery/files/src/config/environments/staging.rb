3f8a38c99272265ea6b5cabbd3f446bd5ff2dcce
Mike Kanning <mkanning@upenn.edu>
2022-05-13 09:54:32 -0400

    fix rubocop error and clarify ActiveJob adapter message in staging.rb

--- ansible/roles/finding_aid_discovery/files/src/config/environments/staging.rb
+++ ansible/roles/finding_aid_discovery/files/src/config/environments/staging.rb
@@ -10,8 +10,8 @@ Rails.application.configure do
   # Set hostname for urls generated within emails
   config.action_mailer.default_url_options = { host: 'pacscl-staging.library.upenn.edu' }
 
-  # Use a real queuing backend for Active Job (and separate queues per environment).
+  # Use sidekiq in staging
   config.active_job.queue_adapter = :sidekiq
-  
+
   config.credentials.key_path = '/run/secrets/rails_master_key'
 end
