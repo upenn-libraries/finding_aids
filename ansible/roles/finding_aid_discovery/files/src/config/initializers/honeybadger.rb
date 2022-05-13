3ae137b37375e41be5a9fc13dea1972967f875d3
Carla Galarza <cgalarza@upenn.edu>
2022-05-13 10:18:04 -0400

    Fixing second rubocop offense

--- ansible/roles/finding_aid_discovery/files/src/config/initializers/honeybadger.rb
+++ ansible/roles/finding_aid_discovery/files/src/config/initializers/honeybadger.rb
@@ -1,5 +1,6 @@
-# Adding Honeybadger configuration.
+# frozen_string_literal: true
 
+# Adding Honeybadger configuration.
 if Rails.env.production? || Rails.env.staging?
   Honeybadger.configure do |config|
     config.api_key = SecretsService.lookup(key: 'honeybadger_api_key')
