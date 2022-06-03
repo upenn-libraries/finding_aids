54f40fc923593a17dc33b562a656616a13d7a6c7
Mike Kanning <mkanning@upenn.edu>
2022-06-03 12:00:12 -0400

    for Endpoints of the penn_aspace type, show a deeplink into Penn ASpace for a harvested record that had an error during extraction or parsing

--- ansible/roles/finding_aid_discovery/files/src/app/helpers/endpoints_helper.rb
+++ ansible/roles/finding_aid_discovery/files/src/app/helpers/endpoints_helper.rb
@@ -8,4 +8,34 @@ module EndpointsHelper
 
     "#{time_ago_in_words DateTime.parse(date), include_seconds: true} ago"
   end
+
+  # @param [Endpoint] endpoint
+  # @param [Hash] file
+  # @return [String]
+  def error_message_for(endpoint, file)
+    if endpoint.penn_aspace_type?
+      aspace_link = link_to_penn_aspace_record(
+        endpoint.harvest_config['repository_id'], file['id']
+      )
+      "#{aspace_link}: #{file['errors'].join(', ')}"
+    else
+      "#{file['id']}: #{file['errors'].join(', ')}"
+    end
+  end
+
+  # return a URL to a record in ASpace based on environment
+  # TODO: as ASpace stuff is generalized, move this into that library
+  # see: https://gitlab.library.upenn.edu/pacscl/finding-aid-discovery/-/issues/93
+  def link_to_penn_aspace_record(endpoint_aspace_id, record_id)
+    return '' if record_id.blank? || endpoint_aspace_id.blank?
+
+    base_url = if Rails.env.production?
+                 'https://upennstaff.as.atlas-sys.com'
+               else
+                 'https://upennsbstaff.as.atlas-sys.com'
+               end
+    link_to(record_id,
+            "#{base_url}/resolve/edit?uri=/repositories/#{endpoint_aspace_id}/resources/#{record_id}",
+            target: '_blank', rel: 'noopener')
+  end
 end
