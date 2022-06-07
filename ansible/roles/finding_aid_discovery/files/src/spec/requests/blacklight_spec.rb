b1c418190ab8a9365ae4097b3695d947fb80d0d8
Mike Kanning <mkanning@upenn.edu>
2022-06-07 18:43:27 -0400

    add xml slr field type. add xml content to this field and include this field in the search fields for the default request handler.

--- ansible/roles/finding_aid_discovery/files/src/spec/requests/blacklight_spec.rb
+++ ansible/roles/finding_aid_discovery/files/src/spec/requests/blacklight_spec.rb
@@ -13,29 +13,31 @@ describe 'Blacklight' do
 
   context 'with a record in the index' do
     let(:solr) { SolrService.new }
-    let(:document_hash) { FactoryBot.attributes_for :solr_document }
+    let(:document_hash) { attributes_for :solr_document, :with_collection_data }
+    let(:document_title) { CGI.escape document_hash[:title_tsi] }
+
     before do
       solr.add_many documents: [document_hash]
       solr.commit
     end
 
-    it 'shows the record in results with a title search' do
-      get "/?q=#{document_hash[:title_tsi]}"
-      expect(response.body).to include document_hash[:title_tsi]
-    end
-
-    xit 'shows the record in results with an identifier search' do
-      get "/?q=#{document_hash[:unit_id_ssi]}"
-      expect(response.body).to include document_hash[:title_tsi]
-    end
-
-    xit 'shows the record in results with an search of some text in a <c> node' do
-      get "/?q=#{document_hash[:unit_id_ssi]}"
-      expect(response.body).to include document_hash[:title_tsi]
-    end
-
     after do
       solr.delete_by_endpoint 'test-endpoint'
+    end
+
+    it 'shows the record in results with a title search' do
+      get "/?q=#{ document_hash[:title_tsi]}"
+      expect(response.body).to include document_hash[:title_tsi]
+    end
+
+    it 'shows the record in results with an identifier search' do
+      get "/?q=#{document_hash[:unit_id_ssi]}"
+      expect(response.body).to include document_hash[:title_tsi]
+    end
+
+    it 'shows the record in results with an search of some text in a <c> node' do
+      get "/?q=Something Really Distinctive"
+      expect(response.body).to include document_hash[:title_tsi]
     end
   end
 end
