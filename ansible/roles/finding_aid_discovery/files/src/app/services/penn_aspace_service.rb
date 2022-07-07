5560ca23b7b82424d5841e71f773b166afa15c91
Mike Kanning <mkanning@upenn.edu>
2022-07-07 07:48:54 -0400

    only harvest published items from ASpace. factor out ASpace API wrapper from ASpace extractor class and add specs.

---
+++ ansible/roles/finding_aid_discovery/files/src/app/services/penn_aspace_service.rb
@@ -0,0 +1,46 @@
+# frozen_string_literal: true
+
+# simple wrapper for the gem-provided ASpace API
+class PennAspaceService
+  # @param [String] repository_id
+  def initialize(repository_id)
+    client.repository(repository_id)
+  end
+
+  # Get IDs for all published resources in the repository
+  # It would be nice if we could use the `all_ids: true` functionality of the API
+  # while also querying only for publish: true items
+  # @return [Array]
+  def all_resource_ids
+    resources = client.resources # Enumerator.lazy here
+    published_resources = resources.select { |r| r['publish'] == true }
+    # there's no ID in the response, but we can extract it from the URI
+    published_resources.map { |r| r['uri'].split('/').last }.to_a
+  end
+
+  # @param [String] resource_id
+  # @return [String]
+  def resource_ead_xml(resource_id)
+    params = { include_unpublished: false, include_daos: true }
+    client.get("resource_descriptions/#{resource_id}.xml?#{params.to_param}").body
+  end
+
+  private
+
+  def client
+    @client ||= ArchivesSpace::Client.new(config).login
+  end
+
+  def config
+    ArchivesSpace::Configuration.new(
+      {
+        base_uri: 'https://upennapi.as.atlas-sys.com',
+        base_repo: '',
+        username: SecretsService.lookup(key: 'penn_aspace_api_username'),
+        password: SecretsService.lookup(key: 'penn_aspace_api_password'),
+        page_size: 50, throttle: 0.1,
+        debug: false, verify_ssl: false
+      }
+    )
+  end
+end
