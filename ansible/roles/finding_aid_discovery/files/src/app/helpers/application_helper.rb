ece7851b7a88e219bced4eab67cca02db67281ec
Mike Kanning <mkanning@upenn.edu>
2023-04-20 09:34:31 -0400

    use a helper to render multiple extent values in an unordered list

--- ansible/roles/finding_aid_discovery/files/src/app/helpers/application_helper.rb
+++ ansible/roles/finding_aid_discovery/files/src/app/helpers/application_helper.rb
@@ -2,9 +2,20 @@
 
 module ApplicationHelper
   # Custom helper method use by Blacklight to truncate abstract.
+  # @param [Hash] options
+  # @return [ActiveSupport::SafeBuffer]
   def truncated_abstract(options)
     truncate(options[:value].first, length: 1_000, separator: ' ') do
       link_to '(see more)', solr_document_path(options[:document])
     end
+  end
+
+  # @param [Hash] options
+  # @return [ActiveSupport::SafeBuffer]
+  def unordered_list(options)
+    list_items = options[:value].map do |item|
+      content_tag :li, item
+    end
+    content_tag :ul, list_items.join.html_safe
   end
 end
