945b5d8f6f65e79c15b1648d667fd1492d260182
Patrick Perkins <lilblizzard97@gmail.com>
2023-09-26 16:13:49 -0400

    magic comment and top level documentation

--- ansible/roles/finding_aid_discovery/files/src/app/controllers/static_pages_controller.rb
+++ ansible/roles/finding_aid_discovery/files/src/app/controllers/static_pages_controller.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: true
+
+# Controller for static pages `about` and `how-to-use`
 class StaticPagesController < ApplicationController
   def about
     render 'static_pages/about'
