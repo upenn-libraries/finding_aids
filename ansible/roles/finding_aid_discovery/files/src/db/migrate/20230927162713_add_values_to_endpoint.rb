0ec897cc391fad1e079b71abec3d934050906355
Patrick Perkins <lilblizzard97@gmail.com>
2023-09-28 12:39:33 -0400

    migration

---
+++ ansible/roles/finding_aid_discovery/files/src/db/migrate/20230927162713_add_values_to_endpoint.rb
@@ -0,0 +1,7 @@
+class AddValuesToEndpoint < ActiveRecord::Migration[7.0]
+  def change
+    add_column :endpoints, :type, :string
+    add_column :endpoints, :url, :text
+    add_column :endpoints, :aspace_id, :integer
+  end
+end
