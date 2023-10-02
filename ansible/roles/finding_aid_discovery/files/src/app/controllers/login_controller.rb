04fc0f0d79d1d589e201aac6d9bd2892ecc7a32a
Mike Kanning <mkanning@upenn.edu>
2023-10-02 13:37:57 +0000

    Hide /login endpoint in production

--- ansible/roles/finding_aid_discovery/files/src/app/controllers/login_controller.rb
+++ ansible/roles/finding_aid_discovery/files/src/app/controllers/login_controller.rb
@@ -2,5 +2,11 @@
 
 # handles requests from user login entrypoint
 class LoginController < ApplicationController
-  def index; end
+  def index
+    if Rails.env.production?
+      head :not_found
+    else
+      render :index
+    end
+  end
 end
