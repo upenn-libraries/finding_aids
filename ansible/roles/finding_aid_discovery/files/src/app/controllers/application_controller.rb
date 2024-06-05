ab712f6675f702f3a16c378cd6d6f3ccc1abbcd2
andrewnk <andrewnk@upenn.edu>
2024-06-04 21:39:10 -0400

    Set up souin caching with traefik

--- ansible/roles/finding_aid_discovery/files/src/app/controllers/application_controller.rb
+++ ansible/roles/finding_aid_discovery/files/src/app/controllers/application_controller.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  # Set cache headers for all controllers
+  before_action -> { expires_in 12.hours, public: true }
+
   # Adds a few additional behaviors into the application controller
   include Blacklight::Controller
   layout :determine_layout if respond_to? :layout
