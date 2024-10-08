dd5b968e33125260bb95ed5a885df41b4618a2e1
Mike Kanning <mkanning@upenn.edu>
2024-10-08 11:13:25 -0400

    be consistent in method parameter style

--- rails_app/app/controllers/api_controller.rb
+++ rails_app/app/controllers/api_controller.rb
@@ -21,23 +21,26 @@ class ApiController < ApplicationController
 
   private
 
-  # @param [String] facet_field
-  # @return [Array<Hash>] ????
+  # @param facet_field [String]
+  # @return [Array<Hash>]
   def facet_response(facet_field:)
     facet_config = blacklight_config.facet_fields[facet_field]
     response = search_service.facet_field_response(facet_config.key, { "f.#{facet_field}.facet.limit" => -1 })
     field_data = response.aggregations[facet_field]
-    field_data.items.map { |entry| hashify_with_link(facet_field, entry) }
+    field_data.items.map { |entry| hashify_with_link(field: facet_field, item: entry) }
   end
 
-  # @param [String] facet_field
-  # @param [Blacklight::Solr::Response::Facets::FacetItem] facet_item
+  # @param field [String]
+  # @param item [Blacklight::Solr::Response::Facets::FacetItem]
   # @return [Hash{Symbol->String}]
-  def hashify_with_link(facet_field, facet_item)
-    { name: facet_item.value, count: facet_item.hits,
-      records_url: facet_entry_url(entry: facet_item.value, field: facet_field) }
+  def hashify_with_link(field:, item:)
+    { name: item.value, count: item.hits,
+      records_url: facet_entry_url(entry: item.value, field: field) }
   end
 
+  # @param entry [String]
+  # @param field [String]
+  # @return [String]
   def facet_entry_url(entry:, field:)
     search_catalog_url("f[#{field}][]" => entry)
   end
