2c43fd3d7c1bb86d02ada9bf45bd0385a046a9a1
Mike Kanning <mkanning@upenn.edu>
2025-02-05 11:28:41 -0500

    include genre/form value in JSON API index responses

--- rails_app/app/controllers/catalog_controller.rb
+++ rails_app/app/controllers/catalog_controller.rb
@@ -123,6 +123,7 @@ class CatalogController < ApplicationController
     config.add_index_field 'extent_ssim', label: I18n.t('fields.extent')
     config.add_index_field 'display_date_ssim', label: I18n.t('fields.date'), if: :json_request?
     config.add_index_field 'subjects_ssim', label: I18n.t('fields.topics.subjects'), if: :json_request?
+    config.add_index_field 'genre_form_ssim', label: I18n.t('fields.genre_form'), if: :json_request?
     config.add_index_field 'abstract_scope_contents_tsi', label: I18n.t('fields.abstract_scope_contents'),
                                                           helper_method: :truncated_abstract
 
