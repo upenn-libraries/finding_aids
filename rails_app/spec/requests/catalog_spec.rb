4912811a577918c2bc260ff20e7896c6074453e4
Mike Kanning <mkanning@upenn.edu>
2025-02-05 17:31:21 -0500

    add spec to ensure we continue to return the expected API index fields

--- rails_app/spec/requests/catalog_spec.rb
+++ rails_app/spec/requests/catalog_spec.rb
@@ -2,9 +2,32 @@
 
 require 'rails_helper'
 
-describe 'Penn vanity route' do
-  it 'routes to the search results with the penn source facet applied' do
-    get upenn_path
-    expect(response).to redirect_to search_catalog_path({ 'f[record_source][]': 'upenn' })
+describe 'Catalog actions' do
+  describe 'UPenn vanity route' do
+    it 'routes to the search results with the penn source facet applied' do
+      get upenn_path
+      expect(response).to redirect_to search_catalog_path({ 'f[record_source][]': 'upenn' })
+    end
+  end
+
+  describe 'JSON API' do
+    let(:data) { response.parsed_body }
+    let(:solr) { SolrService.new }
+    let(:documents) { [attributes_for(:solr_document)] }
+
+    before do
+      solr.add_many documents: documents
+      solr.commit
+    end
+
+    describe 'index endpoint' do
+      before { get search_catalog_path, headers: { 'HTTP_ACCEPT' => 'application/json' } }
+
+      it 'returns the expected attributes' do
+        expected_keys = %w[title extent_ssim display_date_ssim genre_form_ssim creators_ssim
+                           abstract_scope_contents_tsi]
+        expect(data['data'].first['attributes'].keys).to include(*expected_keys)
+      end
+    end
   end
 end
