7844d8ee8ba5e4dc37b7a5ec37f32b646fc2e57d
Amrey Mathurin <mamrey@upenn.edu>
2025-12-03 11:31:02 -0500

    Address rubocop

--- rails_app/config/importmap.rb
+++ rails_app/config/importmap.rb
@@ -1,8 +1,9 @@
+# frozen_string_literal: true
+
 # Pin npm packages by running ./bin/importmap
 
-pin "application", preload: true
-pin "jquery", to: "jquery3.min.js", preload: true
-pin "jquery_ujs", to: "jquery_ujs.js", preload: true
+pin 'application', preload: true
+pin 'jquery', to: 'jquery3.min.js', preload: true
+pin 'jquery_ujs', to: 'jquery_ujs.js', preload: true
 # bundled bootstrap package that includes popper peer dependency
-pin "bootstrap", to: "https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js", preload: true
-
+pin 'bootstrap', to: 'https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js', preload: true
