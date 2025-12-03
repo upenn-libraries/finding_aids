4619fcb6593869e43e392cd15e55df78859a3a1e
Patrick Perkins <pperkins@upenn.edu>
2025-12-03 11:02:10 -0500

    implement faraday

--- rails_app/app/services/download_service.rb
+++ rails_app/app/services/download_service.rb
@@ -1,15 +1,23 @@
 # frozen_string_literal: true
 
-require 'open-uri'
+require 'faraday'
 
 # downloads files, setting appropriate headers and retrying as needed
 class DownloadService
   HEADERS = { 'User-Agent' => 'PACSCL Discovery harvester' }.freeze
+  class DownloadServiceError < StandardError; end
 
   # @param [String] url
   def self.fetch(url)
-    Retryable.retryable(tries: 3, sleep: 6, on: OpenURI::HTTPError) do
-      URI.parse(url).read(HEADERS)
+    connection = Faraday.new do |f|
+      f.request :retry,
+                max: 3,
+                interval: 6,
+                exceptions: Faraday::Retry::Middleware::DEFAULT_EXCEPTIONS + [Faraday::ConnectionFailed]
     end
+    response = connection.get(url, {}, HEADERS)
+    return response.body if response.success?
+
+    raise DownloadServiceError, "Download service failed: #{response.status} - #{response.body}"
   end
 end
--- rails_app/spec/services/download_service_spec.rb
+++ rails_app/spec/services/download_service_spec.rb
@@ -3,13 +3,21 @@
 require 'rails_helper'
 
 describe DownloadService do
-  describe '.fetch' do
-    let(:url) { 'https://www.example.com' }
+  let(:url) { 'https://www.example.com' }
 
+  describe '.fetch' do
     it 'sets appropriate headers' do
-      stub = stub_request(:get, url).with(headers: { 'User-Agent' => 'PACSCL Discovery harvester' })
+      stub = stub_request(:get, url).with(headers: described_class::HEADERS)
       described_class.fetch(url)
       expect(stub).to have_been_made.once
+    end
+
+    context 'when request is unsuccessful' do
+      it 'raises error' do
+        stub_request(:get, url).with(headers: described_class::HEADERS)
+                               .to_return(status: 500)
+        expect { described_class.fetch(url) }.to raise_error(described_class::DownloadServiceError)
+      end
     end
   end
 end
