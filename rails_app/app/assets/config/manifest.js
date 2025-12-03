c8fd1c9b1513d74ff0a4f21df6c04bb24588475a
Amrey Mathurin <mamrey@upenn.edu>
2025-12-03 14:41:04 -0500

    Move precompile assets to sprockets manifest file for easier migration in the future

--- rails_app/app/assets/config/manifest.js
+++ rails_app/app/assets/config/manifest.js
@@ -1,4 +1,7 @@
 //= link_tree ../images
 //= link_directory ../stylesheets
+//= link jquery3.min.js
+//= link jquery_ujs.js
+//= link blacklight/manifest.js
 //= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
