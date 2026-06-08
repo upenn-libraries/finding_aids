5070f9d56ed4cc76d84fd8bcc43b4164f6a1d9ea
Amrey Mathurin <mamrey@upenn.edu>
2026-06-08 10:11:41 -0400

    Restore default blacklight show page

--- rails_app/app/controllers/catalog_controller.rb
+++ rails_app/app/controllers/catalog_controller.rb
@@ -41,7 +41,6 @@ class CatalogController < ApplicationController
 
     # Use local Document component to customize results and show page views
     config.index.document_component = Catalog::ResultsDocumentComponent
-    config.show.document_component = Catalog::ShowDocumentComponent
 
     # Use custom DocumentTitleComponent on results page
     config.index.title_component = Catalog::DocumentTitleComponent
