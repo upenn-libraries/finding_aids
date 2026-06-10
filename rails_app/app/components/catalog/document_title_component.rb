f93cef9e047f352955f0bd657202ee9ccba14360
Amrey Mathurin <mamrey@upenn.edu>
2026-06-10 09:34:50 -0400

    update override documentation

--- rails_app/app/components/catalog/document_title_component.rb
+++ rails_app/app/components/catalog/document_title_component.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module Catalog
-  # Copied from Blacklight 8.12.3 to enable customization of header/title for results page
+  # Copied from Blacklight 9.0.0 to enable customization of header/title for results page
   class DocumentTitleComponent < Blacklight::DocumentTitleComponent
     def title
       text = presenter.heading
