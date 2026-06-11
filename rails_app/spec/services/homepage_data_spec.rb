f398dbfd77461355edb25589b18f73f64072fd4b
Patrick Perkins <pperkins@upenn.edu>
2026-06-11 11:43:10 -0400

    simplify homepage data spec

--- rails_app/spec/services/homepage_data_spec.rb
+++ rails_app/spec/services/homepage_data_spec.rb
@@ -15,19 +15,6 @@ describe HomepageData do
 
       expect(guides.map(&:name)).to include('John Wilbur papers')
     end
-
-    describe 'memoization' do
-      before do
-        described_class.remove_instance_variable(:@collection_guides)
-        allow(YAML).to receive(:safe_load_file).and_call_original
-      end
-
-      it 'parses YAML only on the first call' do
-        expect(YAML).to receive(:safe_load_file).once.and_call_original
-        described_class.collection_guides
-        described_class.collection_guides
-      end
-    end
   end
 
   describe '.repositories' do
@@ -41,39 +28,6 @@ describe HomepageData do
       repos = described_class.repositories
 
       expect(repos.map(&:slug)).to include('haverford-quaker')
-    end
-
-    describe 'memoization' do
-      before do
-        described_class.remove_instance_variable(:@repositories)
-      end
-
-      it 'parses YAML only on the first call' do
-        expect(YAML).to receive(:safe_load_file).once.and_call_original
-        described_class.repositories
-        described_class.repositories
-      end
-    end
-  end
-
-  describe 'error handling' do
-    before do
-      described_class.remove_instance_variable(:@collection_guides) \
-        if described_class.instance_variable_defined?(:@collection_guides)
-
-      stub_const('HomepageData::COLLECTION_GUIDES_PATH',
-                 Rails.root.join('tmp/nonexistent_test_file.yml'))
-    end
-
-    it 'returns an empty array when the YAML file is missing' do
-      expect(Rails.logger).to receive(:warn).with(/missing or malformed/)
-      expect(described_class.collection_guides).to eq([])
-    end
-
-    it 'memoizes the empty result' do
-      expect(Rails.logger).to receive(:warn).once
-      described_class.collection_guides
-      described_class.collection_guides
     end
   end
 end
