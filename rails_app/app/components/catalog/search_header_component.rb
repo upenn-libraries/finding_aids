a5bc1bf06da0008f9d83fe4595e563840bd8f95c
Mike Kanning <mkanning@upenn.edu>
2026-06-15 15:51:44 -0400

    Refactor `SearchHeaderComponent`: consolidate constraints and sort rendering, remove spellcheck suggestions area, and use local Catalog::StartOverButtonComponent.

--- rails_app/app/components/catalog/search_header_component.rb
+++ rails_app/app/components/catalog/search_header_component.rb
@@ -1,7 +1,27 @@
 # frozen_string_literal: true
 
-# Overriding component from Blacklight v9.0.0 for custom Search Header
+# Overriding component from Blacklight v9.0.0 to support:
+#   - combination of constraints/sort into one nav
+#   - removal of "did you mean" spellcheck suggestions area
+#   - usage of local StartOverButton component
 module Catalog
   class SearchHeaderComponent < Blacklight::SearchHeaderComponent
+    def call
+      tag.nav(class: 'fa-constraints-and-sort') do
+        render(constraints) + render('sort_and_per_page')
+      end
+    end
+
+    private
+
+    def constraints
+      helpers.blacklight_config
+             .view_config(helpers.document_index_view_type)
+             .constraints_component
+             .new(
+                search_state: helpers.search_state,
+                start_over_component: Catalog::StartOverButtonComponent
+             )
+    end
   end
 end
