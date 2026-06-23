13998992c980a4b2c61eb3995fff1b5bd3ef5af9
Patrick Perkins <pperkins@upenn.edu>
2026-06-23 14:05:35 -0400

    geocoding, homepage data improvements

--- rails_app/spec/services/homepage_data_spec.rb
+++ rails_app/spec/services/homepage_data_spec.rb
@@ -4,10 +4,30 @@ require 'rails_helper'
 
 describe HomepageData do
   before do
-    # Reset memoized instance variables between tests
     described_class.instance_variable_set(:@collection_guides, nil)
     described_class.instance_variable_set(:@repositories, nil)
+    described_class.instance_variable_set(:@coordinates_cache, nil)
   end
+
+  let(:geocoder_result) do
+    Struct.new(:latitude, :longitude, :coordinates).new(40.0087, -75.3068, [40.0087, -75.3068])
+  end
+
+  let(:facet_data) do
+    [
+      { name: 'Haverford College Quaker & Special Collections', count: 2100 },
+      { name: 'Historical Society of Pennsylvania', count: 300 }
+    ]
+  end
+
+  let(:address_data) do
+    {
+      'Haverford College Quaker & Special Collections' => '370 Lancaster Ave, Haverford, PA 19041',
+      'Historical Society of Pennsylvania' => '1300 Locust St, Philadelphia, PA 19107'
+    }
+  end
+
+  # Collection guides (YAML) -------------------------------------------------
 
   describe '.collection_guides' do
     it 'returns an array of CollectionGuide objects' do
@@ -54,32 +74,105 @@ describe HomepageData do
     end
   end
 
+  # Repositories ------------------------------------------------------------
+
   describe '.repositories' do
+    before do
+      allow(RepositoryQueries).to receive_messages(facet_counts: facet_data, addresses: address_data)
+      allow(Geocoder).to receive(:search).and_return([geocoder_result])
+    end
+
     it 'returns an array of Repository objects' do
       repos = described_class.repositories
 
       expect(repos).to all(be_a(HomepageData::Repository))
     end
 
-    it 'includes repository slugs from the YAML file' do
+    it 'builds Repository structs from query data' do
       repos = described_class.repositories
 
-      expect(repos.map(&:slug)).to include('haverford-quaker')
+      haverford = repos.find { |r| r.slug == 'haverford-college-quaker-special-collections' }
+      expect(haverford.name).to eq('Haverford College Quaker & Special Collections')
+      expect(haverford.count).to eq(2100)
     end
 
-    it 'includes repository counts from the YAML file' do
+    it 'geocodes coordinates from addresses' do
       repos = described_class.repositories
 
-      expect(repos.map(&:count)).to include(2065)
+      haverford = repos.find { |r| r.slug == 'haverford-college-quaker-special-collections' }
+      expect(haverford.lat).to eq(40.0087)
+      expect(haverford.lng).to eq(-75.3068)
     end
 
-    context 'when YAML file is missing' do
+    it 'generates a slug from the name' do
+      repos = described_class.repositories
+
+      expect(repos.map(&:slug)).to include('haverford-college-quaker-special-collections')
+    end
+
+    context 'when a repository has no address' do
       before do
-        allow(YAML).to receive(:safe_load_file).and_raise(Errno::ENOENT)
+        allow(RepositoryQueries).to receive(:addresses).and_return({})
       end
 
-      it 'returns an empty array' do
-        expect(described_class.repositories).to eq([])
+      it 'returns nil coordinates' do
+        repos = described_class.repositories
+        haverford = repos.find { |r| r.slug == 'haverford-college-quaker-special-collections' }
+
+        expect(haverford.lat).to be_nil
+        expect(haverford.lng).to be_nil
+      end
+    end
+
+    context 'when geocoding returns no results' do
+      before do
+        allow(Geocoder).to receive(:search).and_return([])
+      end
+
+      it 'returns nil coordinates' do
+        repos = described_class.repositories
+        haverford = repos.find { |r| r.slug == 'haverford-college-quaker-special-collections' }
+
+        expect(haverford.lat).to be_nil
+        expect(haverford.lng).to be_nil
+      end
+    end
+
+    context 'when geocoding raises an error' do
+      before do
+        allow(Geocoder).to receive(:search).and_raise(Geocoder::Error, 'rate limited')
+      end
+
+      it 'returns nil coordinates' do
+        repos = described_class.repositories
+        haverford = repos.find { |r| r.slug == 'haverford-college-quaker-special-collections' }
+
+        expect(haverford.lat).to be_nil
+        expect(haverford.lng).to be_nil
+      end
+    end
+
+    context 'when the address lookup fails' do
+      before do
+        allow(RepositoryQueries).to receive(:addresses).and_raise(StandardError, 'Solr error')
+      end
+
+      it 'returns nil coordinates' do
+        repos = described_class.repositories
+        haverford = repos.find { |r| r.slug == 'haverford-college-quaker-special-collections' }
+
+        expect(haverford.lat).to be_nil
+        expect(haverford.lng).to be_nil
+      end
+    end
+
+    context 'when Solr is unavailable' do
+      before do
+        allow(RepositoryQueries).to receive(:facet_counts).and_raise(SocketError, 'Connection refused')
+      end
+
+      it 'lets the error propagate' do
+        expect { described_class.repositories }.to raise_error(SocketError)
       end
     end
   end
