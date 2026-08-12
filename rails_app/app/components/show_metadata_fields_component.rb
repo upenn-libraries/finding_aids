9acc13a7f0a679df3b665344c2d3cc59b04514fb
Mike Kanning <mkanning@upenn.edu>
2026-08-12 11:24:35 -0400

    Allow for optional truncation of a displayed list of heading values by the ShowMetadataFieldsComponent. Configure the display of "Creators" in the overview section to show no more than 5 entries, with a link to all of the displayed entries, now also rendered in the "Subject and Headings" section.

--- rails_app/app/components/show_metadata_fields_component.rb
+++ rails_app/app/components/show_metadata_fields_component.rb
@@ -4,15 +4,20 @@
 class ShowMetadataFieldsComponent < ViewComponent::Base
   attr_reader :fields
 
+  LIST_LENGTH_LIMIT = 5
+  HEADINGS_SECTION_ID = '#topics'
+
   # @param fields [Enumerator<Blacklight::FieldPresenter>]
   # @param dl_class [String]
   # @param dt_class [String, nil]
   # @param wrapper_tag [String, Symbol]
-  def initialize(fields:, dl_class: 'pl-dl', dt_class: nil, wrapper_tag: nil)
+  # @param truncate [Boolean] limit displayed lists to LIST_LENGTH_LIMIT
+  def initialize(fields:, dl_class: 'pl-dl', dt_class: nil, wrapper_tag: nil, truncate: false)
     @fields = fields
     @dl_class = dl_class
     @dt_class = dt_class
     @wrapper_tag = wrapper_tag
+    @truncate = truncate
   end
 
   # @return [ActionView::OutputBuffer, ActiveSupport::SafeBuffer]
@@ -20,6 +25,16 @@ class ShowMetadataFieldsComponent < ViewComponent::Base
     return capture(&block) unless @wrapper_tag
 
     content_tag(@wrapper_tag, &block)
+  end
+
+  # @param field [Blacklight::FieldPresenter]
+  # @return [Array]
+  def truncated_headings_list(field:)
+    values = field.render
+    return values if !@truncate || values.length <= LIST_LENGTH_LIMIT
+
+    see_all_link = link_to t('show.sections.overview.see_all_entries', field: field.label), HEADINGS_SECTION_ID
+    values.first(LIST_LENGTH_LIMIT - 1) << see_all_link
   end
 
   # @return [Boolean]
