fd22d450839cdafe7d2b2e1924980dc82e46f4e3
Mike Kanning <mkanning@upenn.edu>
2026-08-28 09:51:45 -0400

    add method docs for HomepageData.hero_images

--- rails_app/app/services/homepage_data.rb
+++ rails_app/app/services/homepage_data.rb
@@ -11,6 +11,7 @@ module HomepageData
   Repository = Data.define(:name, :slug, :count, :lat, :lng, :records_url)
 
   class << self
+    # @return [Config::Options] homepage data from config
     def hero_images
       Settings.homepage_images
     end
