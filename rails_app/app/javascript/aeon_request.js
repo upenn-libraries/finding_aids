3c6e667772a417afb1cc298bbc10c3b37d5b386c
Mike Kanning <mkanning@upenn.edu>
2026-09-15 17:05:45 -0400

    simplify AeonRequest code since container name feature is not desired; ensure UserReview param is always set regardless of RequestType

--- rails_app/app/javascript/aeon_request.js
+++ rails_app/app/javascript/aeon_request.js
@@ -1,11 +1,8 @@
 export default class AeonRequest {
-    constructor(configData, formData = new FormData(),
-                joinVolumes = true, includeTitles = true) {
+    constructor(configData, formData = new FormData()) {
         this.items = 0;
         this.formData = formData;
         this.configData = configData;
-        this.joinVolumes = joinVolumes;
-        this.includeTitles = includeTitles;
         this.addConfigFields();
     }
 
@@ -21,6 +18,7 @@ export default class AeonRequest {
         this.formData.append('Sublocation', this.configData.requestSubLocation);
         this.formData.append('CallNumber', this.configData.requestCallNumber || 'n/a');
         this.formData.append('Title', this.configData.requestTitle);
+        this.formData.append('UserReview', 'No');
     }
 
     addScanFulfillmentFields() {
@@ -29,63 +27,45 @@ export default class AeonRequest {
 
     addLoanFulfillmentFields() {
         this.formData.append('RequestType', 'Loan');
-        this.formData.append('UserReview', 'No');
     }
 
     addItems(items) {
-        const processedItems = new Map();
+        const processedItems = {};
 
         items.forEach(item => {
             this.addProcessedItem(processedItems, item);
         });
 
-        for (const item of processedItems.values()) {
+        Object.entries(processedItems).forEach(([volume, data]) => {
             this.items += 1;
             this.formData.append('Request', this.items);
             this.appendItemFields(this.items, {
-                ItemVolume: item.volume,
-                ItemIssue: item.issues.join(', ').slice(0, 255),
-                ItemNumber: item.barcode,
+                ItemVolume: volume,
+                ItemIssue: data.issues.join(', ').slice(0, 255),
+                ItemNumber: data.barcode,
             });
-        }
+        });
     }
 
     addProcessedItem(processedItems, item) {
-        const volume = item.dataset.volume;
-        const issue = this.stripMarkup(
-            this.includeTitles
-                ? `${item.dataset.issue} [${item.dataset.title}]`
-                : item.dataset.issue
-        );
-
-        const key = this.joinVolumes
-            ? volume
-            : Symbol(); // symbol is a arbitrary unique value
-
-        if (!processedItems.has(key)) {
-            processedItems.set(key, {
-                volume,
-                issues: [],
+        if (!processedItems.hasOwnProperty(item.dataset.volume)) {
+            processedItems[item.dataset.volume] = {
+                issues: [item.dataset.issue],
                 barcode: item.dataset.barcode,
                 call_number: item.dataset.call_number
-            });
+            };
         }
 
-        processedItems.get(key).issues.push(issue);
+        processedItems[item.dataset.volume].issues.push(item.dataset.issue);
     }
 
     appendItemFields(index, fields) {
-        for (const [name, value] of Object.entries(fields)) {
+        Object.entries(fields).forEach(([name, value]) => {
             this.formData.append(`${name}_${index}`, value);
-        }
+        })
     }
 
     reset() {
         this.formData = new FormData();
-    }
-
-    stripMarkup(html){
-        let doc = new DOMParser().parseFromString(html, 'text/html');
-        return doc.body.textContent || "";
     }
 }
